fc768d5ea21c80ea0ee223c90ca415fb98a4a4a7
Bill Smith Linux <schmitty2005@gmail.com>
2014-11-01 22:42:17 -0700

    modify morsedict

--- morse_sound/src/main/java/com/schmitty/MorseLearn/sound/morse_player.java
+++ morse_sound/src/main/java/com/schmitty/MorseLearn/sound/morse_player.java
@@ -8,53 +8,53 @@ public class morse_player {
     //whatever combos are according to TSART book.
     Map <String, String > decode_key = new HashMap<String, String>();
     //copy dictionary from c++ project!
-    public java.util.HashMap<Character, String> morsedict = new java.util.HashMap<Character, String>()
+    public java.util.HashMap<Character, String> morsedict = new java.util.HashMap<Character, String>();
     {
        // convert {} to () and , to ; later
-        morsedict.put ("a"," ..");
-        morsedict.put ("a", ".-");
-        morsedict.put ("b", "-...");
-        morsedict.put ("c", "-.-.");
-        morsedict.put("d", "-..");
-        morsedict.put("e", ".");
-        morsedict.put("f", "..-.");
-        morsedict.put("g", "--.");
-        morsedict.put("h", "....");
-        morsedict.put("i", "..");
-        morsedict.put("j", ".---");
-        morsedict.put("k", "-.-");
-        morsedict.put ("l", ".-..");
-        morsedict.put("m", "--");
-        morsedict.put("n", "-.");
-        morsedict.put("o", "---");
-        morsedict.put("p", ".--.");
-        morsedict.put("q", "--.-");
-        morsedict.put("r", ".-.");
-        morsedict.put("s", "...");
-        morsedict.put("t", "-");
-        morsedict.put("u", "..-");
-        morsedict.put("v", "...-");
-        morsedict.put("w", ".--");
-        morsedict.put("x", "-..-");
-        morsedict.put("y", "-.--");
-        morsedict.put("z", "--..");
-        morsedict.put("0", "-----");
-        morsedict.put("1", ".----");
-        morsedict.put("2", "..---");
-        morsedict.put("3", "...--");
-        morsedict.put("4", "....-");
-        morsedict.put("5", ".....");
-        morsedict.put("6", "-....");
-        morsedict.put("7", "--...");
-        morsedict.put("8", "---..");
-        morsedict.put("9", "----.");
-        morsedict.put("?", "..--..");
-        morsedict.put("!", ".-.-");
-        morsedict.put("(", "--..--");
-        morsedict.put (")", ".........");
-        morsedict.put(" ", " ");
-        morsedict.put("@", ".--.-.");
-        morsedict.put("/", "-..-.");
+
+        morsedict.put ('a', ".-");
+        morsedict.put ('b', "-...");
+        morsedict.put ('c', "-.-.");
+        morsedict.put('d', "-..");
+        morsedict.put('e', ".");
+        morsedict.put('f', "..-.");
+        morsedict.put('g', "--.");
+        morsedict.put('h', "....");
+        morsedict.put('i', "..");
+        morsedict.put('j', ".---");
+        morsedict.put('k', "-.-");
+        morsedict.put ('l', ".-..");
+        morsedict.put('m', "--");
+        morsedict.put('n', "-.");
+        morsedict.put('o', "---");
+        morsedict.put('p', ".--.");
+        morsedict.put('q', "--.-");
+        morsedict.put('r', ".-.");
+        morsedict.put('s', "...");
+        morsedict.put('t', "-");
+        morsedict.put('u', "..-");
+        morsedict.put('v', "...-");
+        morsedict.put('w', ".--");
+        morsedict.put('x', "-..-");
+        morsedict.put('y', "-.--");
+        morsedict.put('z', "--..");
+        morsedict.put('0', "-----");
+        morsedict.put('1', ".----");
+        morsedict.put('2', "..---");
+        morsedict.put('3', "...--");
+        morsedict.put('4', "....-");
+        morsedict.put('5', ".....");
+        morsedict.put('6', "-....");
+        morsedict.put('7', "--...");
+        morsedict.put('8', "---..");
+        morsedict.put('9', "----.");
+        morsedict.put('?', "..--..");
+        morsedict.put('!', ".-.-");
+        morsedict.put('(', "--..--");
+        morsedict.put (')', ".........");
+        morsedict.put(' ', " ");
+        morsedict.put('@', ".--.-.");
+        morsedict.put('/', "-..-.");
 
         //finish fixing dictionary
         //add that one command to convert it to unchangeable.....whatever it was called...
