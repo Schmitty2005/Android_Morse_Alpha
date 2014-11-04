01d2b047154c737e108e4ed4da7835b8f297b38c
Bill Smith Linux <schmitty2005@gmail.com>
2014-11-03 20:44:20 -0800

    Added sub routines for dit dah and wave creation. Simple headers.

--- morse_sound/src/main/java/com/schmitty/MorseLearn/sound/morse_player.java
+++ morse_sound/src/main/java/com/schmitty/MorseLearn/sound/morse_player.java
@@ -1,14 +1,54 @@
 package com.schmitty.MorseLearn.sound;
 import java.util.*;
+import java.io.ByteArrayOutputStream; // equivalent to memory stream in vb.net ????
+
 //TODO find vb.net memory stream equivalent for java programs.
 //todo Add dictionary for character levels
 //TODO Create global variables for class
+//TODO copy C++ code into java for wave file creation.
 /** morse_player creates dit and dah waves and allows single character and word playback in morse code.
  *
  * @author Brian S.
  * @version Alpha
  */
-public class morse_player {
+public class morse_player { //( double frequency, int WPM, boolean farnsworth){
+//TODO create WPM variables and wave file variables
+    //variables for dit dah lengths.
+    double dit_duration_ms = 100;
+    double dah_duration_ms = 300;
+    double dit_duration_samples = 0;
+    double dah_duration_samples = 0;
+    double freq_hz = 800;
+    int WPM = 25;
+    boolean farnsworth_space_bool = false;
+    double inter_element_spacing = 50;
+    double word_spacing = 300;
+
+// variables for wave creation
+    double sample_rate = 44100;
+    double channels = 1;
+    double theta = Math.PI * 2;
+
+    /**
+     * Convert PCM wave stream to WAVE file format returned as WAVE_sample
+     * @param PCM_sample PCM based ByteArrayOutputStream (input data)
+     * @param WAVE_sample WAVE file created from PCM_sample (output data)
+     * @return
+     */
+   private ByteArrayOutputStream PCM_to_Wave (ByteArrayOutputStream PCM_sample, ByteArrayOutputStream WAVE_sample)
+   {
+       return WAVE_sample;
+
+   }
+
+
+    private ByteArrayOutputStream hahn_window ( ByteArrayOutputStream PCM_sample, int rise_time, int decay_time ){
+        //TODO function to create a hahn window for the wave file
+        //read full wave and create a hahn window for wave file.
+        //possibly not use this function but will build it into the wave creation routine.
+    return PCM_sample;
+    }
+
 //create map for character to morse code conversion
 //Also create map class for different levels of character learning.  level 1 5etar level 2 sln04 etc..
     //whatever combos are according to TSART book.
@@ -63,5 +103,6 @@ public class morse_player {
         //finish fixing dictionary
         //add that one command to convert it to unchangeable.....whatever it was called...
     }
-
+public ByteArrayOutputStream dit_stream = new ByteArrayOutputStream();
+public ByteArrayOutputStream dah_stream = new ByteArrayOutputStream();
 }
